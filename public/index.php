d46f92dec0724d4f009daaa79c435832b340a4ab
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-20 02:04:06 +0100

    initial commit for public/index.php file creation.

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use GuiBranco\PocMvc\App\Registration;
+use GuiBranco\PocMvc\Src\Application;
+
+$app = new Application();
+$registration = new Registration($app);
+$registration->addServices();
+$registration->registerRoutes();
+$app->run();
