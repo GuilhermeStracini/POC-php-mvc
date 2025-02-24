f8cf694461da3c623eda522bc4ece0fc1833a0b1
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-02-24 19:17:50 +0000

    Update Application.php (#82)

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -14,7 +14,7 @@ class Application
     public function __construct()
     {
         $this->container = new DIContainer();
-        $this->router = new Router($this->container);
+        $this->router = new Router($this->container, 'poc-php-mvc/');
     }
 
     /**
